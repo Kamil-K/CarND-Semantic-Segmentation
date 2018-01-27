d0290ca6409456f8949aaf1936ca5c808d1d3198
Ubuntu <ubuntu@ip-172-31-45-44.us-east-2.compute.internal>
2018-01-27 21:36:52 +0000

    fix: syntax

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-	conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same', kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
-	output = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, 2, padding='same', kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same', kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, 2, padding='same', kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
 	
 	# tf.Print(output, [tf.shape(output)][1:3])
 	tf.Print(output, [tf.shape(output)])
